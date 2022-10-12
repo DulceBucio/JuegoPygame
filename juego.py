85a6310d7e3ec2ae491fd143b357dfa9b914e9b7
DulceBucio <dulcenahomibucio@gmail.com>
2022-10-12 15:47:15 -0500

    soluciones c:

--- juego.py
+++ juego.py
@@ -1,13 +1,10 @@
 from pygame import *
 import sys, time
+import random
 
 init() 
 screen = display.set_mode((800,600)) 
 pirateFont = font.Font("Pirate Ship.ttf", 20)
-activoa1 = False
-activoa2 = False
-activoa3 = False
-
 
 def Inicio(escena):
     global activo
@@ -26,12 +23,12 @@ def Inicio(escena):
     logo = transform.scale(logo, (600, 250))
     mixer.music.load("soundtrack2.mp3")
     mixer.music.play(-1)
+    ins = True 
     while True:
        screen.fill((255,255,255))
        for e in event.get():
            if e.type == QUIT: sys.exit()
-           if e.type == KEYDOWN and K_d: 
-            activo1 = True
+           if e.type == KEYDOWN and K_a: ins = False
        screen.blit(fondo1, (0,0))
        screen.blit(fondo2, (0,100))
        screen.blit(nube1, (50,50))
@@ -40,12 +37,16 @@ def Inicio(escena):
        screen.blit(nube4, (700, 250))
        screen.blit(merry, (xm, 420))
        screen.blit(logo, (100, 0))
-       #bv1 = pirateFont.render("Bienvenido Pirata!", True, (255,255,255))
-       bv2 = pirateFont.render("Avanza para jugar", True, (255,255,255))
-       bv3 = pirateFont.render("Presiona 1 para las instrucciones", True, (255,255,255))
-       #screen.blit(bv1, (170, 100))
-       screen.blit(bv2, (300, 250))
-       screen.blit(bv3, (220, 270))
+       bv1 = pirateFont.render("Avanza para jugar", True, (255,255,255))
+       bv2 = pirateFont.render("Presiona 1 para las instrucciones", True, (255,255,255))
+       bv3 = pirateFont.render("Te enfrentaras a los villanos mas feroces de todo el mar, pero cuidado", True, (255,255,255))
+       bv4 = pirateFont.render("solo puedes derrotarlos cuidadosamente con cierto patron de ataques", True, (255,255,255))
+       if ins:
+        screen.blit(bv1, (300, 250))
+        screen.blit(bv2, (220, 270))
+       else: 
+        screen.blit(bv3, (0,250))
+        screen.blit(bv4, (0,280))
        if key.get_pressed()[K_d]: xm = (xm + 3) 
        if xm > 800: return 2
        display.flip()
@@ -54,7 +55,8 @@ def cargaranim(prefijo,sufijo,n):
     images = []
     for i in range(1, n+1):
         name = prefijo+str(i)+sufijo
-        images.append(image.load(name))
+        anim = transform.scale(image.load(name), (200,200))
+        images.append(anim)
     return images
 
 def mostraranim(images, freq, x, y):
@@ -75,15 +77,25 @@ def Morgan(escena):
     ace_normal = image.load("acetieso1.png")
     ace_normal = transform.scale(ace_normal, (200, 200))
     mixer.music.play(-1)
+    mostrara1 = False
+    teclas = ""
+    num = random.randint(1,2)
+    solucion = ("d"*num + "a"*num) * 3
     while True:
         screen.fill((255,255,255))
         for e in event.get():
             if e.type == KEYDOWN and e.key == K_p: return 3
-            if e.type == KEYDOWN and e.key == K_d: activoa1 = True
+            if e.type == KEYDOWN: 
+                mostrara1 = True
+                teclas = teclas + chr(e.key)
+                print(teclas)
         screen.blit(fondomor, (0,0))
         screen.blit(ace_normal, (200,250))
+        if mostrara1:
+            mostraranim(acem, 10, 300, 350)
+        if teclas == solucion: print("si")
         #load
-        ins1 = pirateFont.render("Pirata, ataca!", True, (255,255,255))
+        ins1 = pirateFont.render(solucion, True, (255,255,255))
         screen.blit(ins1, (300, 10))
         display.flip()
 def Vista(escena):
@@ -92,11 +104,15 @@ def Vista(escena):
     fondovis = transform.scale(fondovis, (800,600))
     mixer.music.load("soundtrack1.mp3")
     mixer.music.play(-1)
+    teclas = ""
+    num = random.randint(1,2)
+    solucion = ("d"*num + "a"*num + "w"*num) * 4
     while True:
         screen.fill((255,255,255))
         for e in event.get():
             if e.type == QUIT: sys.exit()
             if e.type == KEYDOWN and e.key == K_p: return 4
+        if teclas == solucion: print("si")
         screen.blit(fondovis, (0,0))
         ins2 = pirateFont.render("No lo dejes escapar!", True, (255,255,255))
         screen.blit(ins2, (250,10))
@@ -108,6 +124,9 @@ def Enel(escena):
     cielo = transform.scale(cielo,(800,600))
     fondoenel = image.load("fondoenel.png")
     fondoenel = transform.scale(fondoenel, (800,600))
+    teclas = ""
+    num = random.randint(1,2)
+    solucion = ("d"*num + "a"*num + "w"*num + "s") * 5
     while True:
         screen.fill((255,255,255))
         for e in event.get():
